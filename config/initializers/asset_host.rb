55a9c0384ed61736d7eed9c4d5cb05cd99e8a4bf
Andrew Zotov <andrew.zotov@gmail.com>
2008-08-31 02:52:37 +0400

    asset host fix, :80 is no longer appended

--- config/initializers/asset_host.rb
+++ config/initializers/asset_host.rb
@@ -1,6 +1,6 @@
 # map properly for developer-environment
 ActionController::Base.asset_host = Proc.new do |source, request|
-  if request.port == '80'
+  if request.port == 80
     'http://www.mmm-tasty.ru'
   else
     "http://www.mmm-tasty.ru:#{request.port}"
