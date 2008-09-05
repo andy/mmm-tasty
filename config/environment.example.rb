47ea4ff5c80bd4a9c1216c6459a161748412aff1
Andrew Zotov <andrew.zotov@gmail.com>
2008-09-06 00:32:35 +0400

    rails 2.1.1

--- config/environment.example.rb
+++ config/environment.example.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your web server when you modify this file.
 
-RAILS_GEM_VERSION = '2.1.0' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.1.1' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
